509a4aae7f11d79fe4197662ed2b4e26f60184c4
Hamid Sarani <h.sarani75@gmail.com>
2026-03-29 06:43:49 +0330

    CORS

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3033', 'http://localhost:3000'],
+    origin: ['http://localhost:3033', 'http://localhost:3000', 'https://cyber.mardomi.org'],
     credentials: true,
   });
 
